7f0e88a4a91e25186e22392ecc0374a41cf21eb3
Camila Maximo <camilamaximo20@gmail.com>
2024-03-30 16:13:30 +0000

    first commit

---
+++ Operadores/js/scripts.js
@@ -0,0 +1,61 @@
+
+
+console.log(typeof 2);
+console.log(typeof true);
+console.log(typeof null);
+
+//aritmética
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 / 2);
+console.log(2 * 2);
+console.log(2 % 2);
+
+//Especial Namber
+//Infinity
+//-Infinity
+//NaN(Not a number)
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(3 * "ted");
+console.log(typeof NaN);
+
+//tipo string
+
+console.log(typeof "oi")
+console.log("Testando a \n quebra de linha")
+console.log("Espaçamento \t de tab")
+
+//concatenanação
+
+console.log("Meu nome é " + "nome")
+
+//Interpolação- template string
+
+console.log(  `A soma de 2 + 2 é: ${2+2} `  )
+
+//Booleans
+
+console.log(true);
+console.log(5 > 20);
+console.log(30 > 10);
+console.log(typeof false);
+
+//comparações
+
+console.log( 5 <= 5)
+console.log(5 < 5)
+console.log(10 == 10)
+console.log(10 == 9)
+console.log(10 != 9)
+
+//valor idêntico
+
+console.log(9 == "9")
+console.log(9 === "9")
+console.log(9 !== "9")
+
+//Operadores lógicos
+//  && AND , || OR , ! not(inverte o valor da operação)
+
+
