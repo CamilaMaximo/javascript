9cf0bd33193ce97ab906514e897c0f093e62ec96
CamilaMaximo <camilamaximo20@gmail.com>
2024-03-30 17:33:24 +0000

    atualização

---
+++ EstruturaDeProgramação/index.js
@@ -0,0 +1,38 @@
+let nome = "Camila"
+console.log(nome)
+
+nome = "Camila Aleixo"
+
+console.log(nome)
+
+// let - uma variável mutavel
+//cont - não é mutavel
+
+//Criar varias variaveis
+
+let a = 4, b = 5 , c = 10
+console.log(a,b,c)
+
+//nomes reservados 
+//não podem utilizar,pois já tem uma definição no javaScript
+
+
+//Estrutura de uma função
+//função prompt
+//const age = prompt("Seu nome ?");
+//console.log(`você tem ${age} idade.`);
+
+// Objeto MATH
+/*
+math - metódo de matemática
+
+max: encontra o maior número
+floor: arredonda pa baixo
+
+*/
+
+console.log(Math.max(5,2,1,10))
+
+console.log(Math.floor(5.14))
+
+console.log(Math.ceil(5.14))
--- Operadores/js/scripts.js
+++ Operadores/js/scripts.js
@@ -58,4 +58,42 @@ console.log(9 !== "9")
 //Operadores lógicos
 //  && AND , || OR , ! not(inverte o valor da operação)
 
+//Operadores lógicos
+console.log(true && true);
+
+console.log(true && false);
+
+console.log(5 > 2 && 2 < 10);
+
+
+console.log(5 > 2 && "Matheus" === 1);
+
+console.log(5 > 2 || "Matheus" === 1); //basta apenas um lado true para o resultado ser true no operador "OU"
+
+console.log(5 > 2  || 5 > 100);
+
+//Empty Values
+// palavras reservadas  - undefined e null
+
+
+console.log(typeof null, typeof undefined);
+console.log(null == false)
+console.log(undefined == false)
+
+//Conversão do tipo automático
+
+/*
+
+5*null = 0
+"5"-3 = 2
+"5"+ 1 = 51 
+"a"* "b"= NaN
+
+*/
+
+
+
+
+
+
 
